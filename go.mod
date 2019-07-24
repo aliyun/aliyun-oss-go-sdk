19cb06df1c029663c9efae8ff8a1ec4a75278edf
charliew <charliew@yeetalk.net>
2019-07-24 16:26:05 +0800

    add AddCallback and AddCallbackVar and signCallbackUrl sample

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aliyun/aliyun-oss-go-sdk
+
+go 1.12
+
+require golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
---
+++ sample/sign_url_test.go
@@ -0,0 +1,10 @@
+package sample
+
+import (
+	"log"
+	"testing"
+)
+
+func Test_signCallbackUrl(t *testing.T) {
+	log.Println(signCallbackUrl())
+}
