59d768b3c1aa2f8fdb831d5b488e573a60512679
yizhihouzi <chtsx@qq.com>
2022-08-11 10:56:47 +0800

    Add x-oss-ac-* into subresource list.

--- oss/conn.go
+++ oss/conn.go
@@ -48,7 +48,7 @@ var signKeyList = []string{"acl", "uploads", "location", "cors",
 	"worm", "wormId", "wormExtend", "withHashContext",
 	"x-oss-enable-md5", "x-oss-enable-sha1", "x-oss-enable-sha256",
 	"x-oss-hash-ctx", "x-oss-md5-ctx", "transferAcceleration",
-	"regionList", "cloudboxes",
+	"regionList", "cloudboxes", "x-oss-ac-source-ip", "x-oss-ac-subnet-mask", "x-oss-ac-vpc-id", "x-oss-ac-forward-allow",
 }
 
 // init initializes Conn
